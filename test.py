5f19f26c94db270e8b027e9d66544621ceee2550
stanley <5127884@qq.com>
2025-09-29 13:26:28 +0800

    Add notes page on branch

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, My Knowledge Base!"
+    notes = "Hello, My knowledge Base!"
+    return render_template('index.html', notes=notes)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
